450bedf47ea0fd5837ca3e86eb4fdea81f035fad
huazheng <hua.zheng@91sports.cc>
2019-12-17 11:49:25 +0800

    添加think_swoole相关配置文件  修改session存储为redis

--- config/session.php
+++ config/session.php
@@ -1,15 +1,14 @@
 <?php
 
 
-/* 定义会话路径 */
-$_path_ = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
-file_exists($_path_) || mkdir($_path_, 0755, true);
-$_name_ = 's' . substr(md5(__DIR__), -8);
-
-/* 配置会话参数 */
 return [
-    'prefix'         => 'qn',
-    'path'           => $_path_,
-    'name'           => $_name_,
-    'var_session_id' => $_name_,
+    'type'       => 'redis',
+    'prefix'     => 'qnopen',
+    'auto_start' => true,
+    // redis主机
+    'host'       => env('redis.master_hostname','127.0.0.1'),
+    // redis端口
+    'port'       => env('redis.master_hostport','6379'),
+    // 密码
+    'password'   =>  env('redis.master_auth',''),
 ];
